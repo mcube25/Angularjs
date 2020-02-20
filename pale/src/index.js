fb24510f03863545d4ecd416ced69947443e19ca
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-02-19 16:34:11 -0800

    new to js

---
+++ pale/src/index.js
@@ -0,0 +1,14 @@
+//constant are use to ddeclare values that are not changeable 
+const a =42;
+//tis value cant be changed
+//let is used witin the scope of the code
+let cardId = 100;
+console.log(cardId);
+//will only work on the scope it is declared
+if (true){
+	let cardId =200
+}
+console.log(carId);
+//will not work because of yhe let 
+//but using the var keyword will work in the above code
+//REST PARAMETERS
