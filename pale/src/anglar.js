d2ea9054ef7f0154df58d72be3713acf356225ef
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-02-20 10:50:35 -0800

    learning angular

---
+++ pale/src/anglar.js
@@ -0,0 +1,13 @@
+//Angular tutorials
+//data binding
+//it is commnication between typescript code(biz logic)
+//and template
+//we outpt data using 
+//1.string interpolation ({{data}})
+//. property Binding ([property]="data");
+//we react to [User] Events by doing
+//event binding ((event)="expression")
+//then we hhave a two way binding
+
+//STRING interpolation
+//property binding;
